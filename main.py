a757db6ee5be6f6be34a79478ae54ef174abd2d0
Rafael Montalvo <rafamont@espol.edu.ec>
2022-01-22 14:00:14 -0500

    Implementacion de los graficos sobre el anio de lanzamiento en el main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 import plataformasGrafico
+import AnioLanzamientoGrafico
 import matplotlib.pyplot as plt
 
 def main():
-    print("Grafico de barras de las plataformas")
+    print("Graficos estadisticos")
 
 
 plataformasGrafico.plataformas()
 plataformasGrafico.clasificacion()
 plataformasGrafico.grafico()
 plt.show()
+#--------Grafico de barras y pastel del anio de lanzamiento-----
+AnioLanzamientoGrafico.anio_lanzamiento()
