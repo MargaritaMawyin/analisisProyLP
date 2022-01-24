3eeeb3758ad6cc4d26891481f547ae2669780c8e
willymateo <wjmateo@espol.edu.ec>
2022-01-23 19:18:25 -0500

    Gráfico orden descendente

--- GeneroGrafico.py
+++ GeneroGrafico.py
@@ -6,6 +6,7 @@ def genero():
     df['numero_juegos'] = 1
 
     df = df.groupby(['genero']).count().numero_juegos
+    df = df.sort_values(ascending= False)
     df.plot(kind="bar",figsize=(10, 7))
     print(df)
 
