15dc5dfb7289e2ee2d4095252c7d20d4fdb0fac4
willymateo <wjmateo@espol.edu.ec>
2022-01-23 19:03:51 -0500

    Actualización de gráfico

--- GeneroGrafico.py
+++ GeneroGrafico.py
@@ -2,13 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def genero():
-    # Se crea nuevo campo para cantidad_juegos
     df = pd.read_csv("./archivos/juegos_general.csv")
-    df['cantidad_juegos'] = 1
+    df['numero_juegos'] = 1
 
-    # Se agrupan por año lanzamiento y se van sumando los juegos de cada año
-    df = df.groupby(['genero']).cantidad_juegos.count().reset_index()
-    df = df.set_index('genero')  # Se utiliza el año de lanzamiento como nuevo indice
-    df.plot(kind="bar", figsize=(8, 7))
+    df = df.groupby(['genero']).count().numero_juegos
+    df.plot(kind="bar",figsize=(10, 7))
     print(df)
+
     plt.show()
